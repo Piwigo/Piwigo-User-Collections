1b8ae6ac18d3ba84b0678627b8b9fe710c43e8b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-12 20:33:31 +0000

    [user_collections] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16663 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Pievienot kolekcijai';
+$lang['Clear collection'] = 'Tīrs';
+$lang['Collection name:'] = 'Kolekcijas nosaukums:';
+$lang['Collection'] = 'Kolekcija';
+$lang['Collections'] = 'Kolekcijas';
+$lang['Copied'] = 'Nokopēts';
+$lang['Copy to clipboard'] = 'Kopēt atmiņā';
+$lang['Create a new collection'] = 'Izveidot jaunu kolekciju';
+$lang['Current collection:'] = 'Esošā kolekcija:';
+$lang['Display collection'] = 'Rādīt';
+$lang['Invalid collection'] = 'Nederīga kolekcija';
+$lang['Please give a name'] = 'Lūdzu dodiet nosaukumu';
+$lang['Public collection'] = 'Publiska';
+$lang['Remove from collection'] = 'Izņemt no kolekcijas';
+$lang['Return to collections list'] = 'Atgriezties pie kolekciju saraksta';
+$lang['Saved collections'] = 'Saglabātās kolekcijas';
+$lang['See all my collections'] = 'Skatīt visas savas kolekcijas';
+$lang['This collection is empty'] = 'Šī kolekcija ir tukša';
+$lang['Unsaved collections'] = 'Nesaglabātās kolekcijas';
+$lang['by %s'] = 'ar %s';
+$lang['save'] = 'saglabāt';
+$lang['set active'] = 'aktivēt';
+?>
