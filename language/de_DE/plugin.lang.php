e8bd056fc9de1524b3d24b1149abbe2f6a5a14c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-12 20:33:20 +0000

    [user_collections] Update de_DE, thanks to : Polly
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16662 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Sammlung hinzufügen';
+$lang['Clear collection'] = 'Löschen';
+$lang['Collection name:'] = 'Name der Sammlung:';
+$lang['Collection'] = 'Sammlung';
+$lang['Collections'] = 'Sammlungen';
+$lang['Copied'] = 'Kopiert';
+$lang['Copy to clipboard'] = 'In die Zwischenablage kopieren';
+$lang['Create a new collection'] = 'Erstelle eine neue Sammlung';
+$lang['Current collection:'] = 'Aktuelle Sammlung:';
+$lang['Display collection'] = 'Anzeigen';
+$lang['Invalid collection'] = 'Falsche Sammlung';
+$lang['Please give a name'] = 'Bitte einen Namen eingeben';
+$lang['Public collection'] = 'Öffentlich';
+$lang['Remove from collection'] = 'Aus der Sammlung entfernen';
+$lang['Return to collections list'] = 'Zurück zur Liste der Sammlungen';
+$lang['Saved collections'] = 'Sammlungen speichern';
+$lang['See all my collections'] = 'Zeige alle meine Sammlungen';
+$lang['This collection is empty'] = 'Diese Sammlung ist leer';
+$lang['Unsaved collections'] = 'Nicht gesicherte Sammlungen';
+$lang['by %s'] = 'von %s';
+$lang['save'] = 'speichern';
+$lang['set active'] = 'aktivieren';
+?>
