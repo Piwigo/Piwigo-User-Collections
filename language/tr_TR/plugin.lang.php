101c15e0711026e45f94fd8dc360ec5639667be4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 06:15:00 +0000

    [user_collections] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16673 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Koleksiyona ekle';
+$lang['Clear collection'] = 'Temizle';
+$lang['Collection name:'] = 'Koleksiyon adı:';
+$lang['Collections'] = 'Koleksiyon';
+$lang['Collection'] = 'Koleksiyon';
+$lang['Copied'] = 'Kopyalandı';
+$lang['Copy to clipboard'] = 'Panoya kopyala';
+$lang['Create a new collection'] = 'Yeni bir koleksiyon oluştur';
+$lang['Current collection:'] = 'Geçerli koleksiyon:';
+$lang['Display collection'] = 'Görüntüle';
+$lang['Invalid collection'] = 'Geçersiz koleksiyon';
+$lang['Please give a name'] = 'Lütfen bir isim verin';
+$lang['Public collection'] = 'Herkese açık';
+$lang['Remove from collection'] = 'Koleksiyondan çıkart';
+$lang['Return to collections list'] = 'Koleksiyon listesine dön';
+$lang['Saved collections'] = 'Kaydedilmiş koleksiyonlar';
+$lang['See all my collections'] = 'Tüm koleksiyonlarımı gör';
+$lang['This collection is empty'] = 'Bu koleksiyon boş';
+$lang['Unsaved collections'] = 'Kaydedilmemiş koleksiyonlar';
+$lang['by %s'] = '%s tarafından';
+$lang['save'] = 'kaydet';
+$lang['set active'] = 'etkin olarak ayarla';
+?>
