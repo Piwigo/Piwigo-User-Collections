b87e6db9941ecdecead8ed15e981a1ad60175aaa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 06:14:46 +0000

    [user_collections] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16672 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Pridať do kolekcie';
+$lang['Clear collection'] = 'Vyčistiť';
+$lang['Collection name:'] = 'Názov kolekcie:';
+$lang['Collection'] = 'Kolekcia';
+$lang['Collections'] = 'Kolekcie';
+$lang['Copied'] = 'Skopírované';
+$lang['Copy to clipboard'] = 'Kopírovať do schránky';
+$lang['Create a new collection'] = 'Vytvoriť novú kolekciu';
+$lang['Current collection:'] = 'Súčasná kolekcia:';
+$lang['Display collection'] = 'Zobraziť';
+$lang['Invalid collection'] = 'Neplatná kolekcia';
+$lang['Please give a name'] = 'Prosím prideľte názov';
+$lang['Public collection'] = 'Verejné';
+$lang['Remove from collection'] = 'Odobrať z kolekcie';
+$lang['Return to collections list'] = 'Návrat do zoznamu kolekcií';
+$lang['Saved collections'] = 'Uložené kolekcie';
+$lang['See all my collections'] = 'Zobraziť všetky moje kolekcie';
+$lang['This collection is empty'] = 'Táto kolekcia je prázdna';
+$lang['Unsaved collections'] = 'Neuložené kolekcie';
+$lang['by %s'] = 'pri %s';
+$lang['save'] = 'uložiť';
+$lang['set active'] = 'označiť ako aktívne';
+?>
