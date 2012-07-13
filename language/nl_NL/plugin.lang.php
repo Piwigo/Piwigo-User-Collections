19dccd476afaee84f6f0c73dc551fe7e317a0240
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 09:38:32 +0000

    [user_collections] Update nl_NL, thanks to : ruud
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16683 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Aan verzameling toevoegen';
+$lang['Clear collection'] = 'Leegmaken';
+$lang['Collection name:'] = 'Naam van verzameling';
+$lang['Collection'] = 'Verzameling';
+$lang['Collections'] = 'Verzamelingen';
+$lang['Copied'] = 'Gekopieerd';
+$lang['Copy to clipboard'] = 'Kopiëren naar clipboard';
+$lang['Create a new collection'] = 'Maak een nieuwe verzameling';
+$lang['Current collection:'] = 'Huidige verzameling';
+$lang['Display collection'] = 'Toon';
+$lang['Invalid collection'] = 'Ongeldige verzameling';
+$lang['Please give a name'] = 'Geef een naam';
+$lang['Public collection'] = 'Openbaar';
+$lang['Remove from collection'] = 'Verwijder uit verzameling';
+$lang['Return to collections list'] = 'Terug naar verzamelingenlijst';
+$lang['Saved collections'] = 'Opgeslagen verzamelingen';
+$lang['See all my collections'] = 'Alle verzamelingen bekijken';
+$lang['This collection is empty'] = 'Deze verzameling is leeg';
+$lang['Unsaved collections'] = 'Niet opgeslagen verzamelingen';
+$lang['by %s'] = 'door %s';
+$lang['save'] = 'Opslaan';
+$lang['set active'] = 'Maak actief';
+?>
