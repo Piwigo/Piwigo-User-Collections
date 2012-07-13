24e3d46dc0fa431968d7a40de52ebf5b8e378992
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 07:08:02 +0000

    [user_collections] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16680 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Προσθήκη στη συλλογή';
+$lang['Clear collection'] = 'Καθαρίζω';
+$lang['Collection name:'] = 'Όνομα Συλλογής';
+$lang['Collection'] = 'Συλλογή';
+$lang['Collections'] = 'Συλλογές';
+$lang['Copied'] = 'Αντιγράφτηκε ';
+$lang['Copy to clipboard'] = 'Αντιγραφή στο πρόχειρο';
+$lang['Create a new collection'] = 'Δημιουργήστε μια νέα συλλογή';
+$lang['Current collection:'] = 'Τρέχουσα συλλογή:';
+$lang['Display collection'] = 'Εμφάνιση';
+$lang['Invalid collection'] = 'Άκυρη συλλογή';
+$lang['Please give a name'] = 'Παρακαλούμε δώστε ένα όνομα';
+$lang['Public collection'] = 'Δημόσιο';
+$lang['Remove from collection'] = 'Κατάργηση από τη συλλογή';
+$lang['Return to collections list'] = 'Επιστροφή στη λίστα με τις συλλογές';
+$lang['Saved collections'] = 'Αποθηκευμένες Συλλογές';
+$lang['See all my collections'] = 'Δείτε όλες τις συλλογές μου';
+$lang['This collection is empty'] = 'Αυτή η συλλογή είναι άδεια';
+$lang['Unsaved collections'] = 'Οι μη αποθηκευμένες συλλογές';
+$lang['by %s'] = 'από %s';
+$lang['save'] = 'αποθηκεύω';
+$lang['set active'] = 'ενεργοποιήστε';
+?>
