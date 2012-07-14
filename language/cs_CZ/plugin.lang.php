c2a8bdb0d6f8e8c784c0a014f53a40f577e967f2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-14 12:07:14 +0000

    [user_collections] Update cs_CZ, thanks to : lanius
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16722 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d more...'] = '%d více...';
+$lang['Add to collection'] = 'Přidat do sbírky';
+$lang['Clear collection'] = 'Vyčistit';
+$lang['Collection name:'] = 'Jméno sbírky';
+$lang['Collection'] = 'Sbírka';
+$lang['Collections'] = 'Sbírky';
+$lang['Copied'] = 'Zkopírováno';
+$lang['Copy to clipboard'] = 'Zkopírovat do schránky';
+$lang['Create a new collection'] = 'Vytvořit novou sbírku';
+$lang['Invalid collection'] = 'Neplatná sbírka';
+$lang['Please give a name'] = 'Prosím pojmenujte';
+$lang['Public collection'] = 'Veřejné';
+$lang['Remove from collection'] = 'Odstranit ze sbírky';
+$lang['Return to collections list'] = 'Návrat do seznamu';
+$lang['Save'] = 'Uložit';
+$lang['Saved collections'] = 'Uložené sbírky';
+$lang['See all my collections'] = 'Vidět všechny mé sbírky';
+$lang['This collection is empty'] = 'Tato sbírka je prázdná';
+$lang['Unsaved collections'] = 'Neuložené sbírky';
+$lang['You have %d collection'] = 'Máte %d sbírku';
+$lang['You have %d collections'] = 'Máte %d sbírky';
+$lang['You have no collection'] = 'Nemáte žádnou sbírku ';
+$lang['active'] = 'aktivní';
+$lang['by %s'] = 'od %s';
+$lang['save'] = 'uložit';
+$lang['set active'] = 'nastavit aktivní';
+?>
