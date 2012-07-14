32a30c8f6ac8320a86038877981ce46894f981af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-14 06:05:40 +0000

    [user_collections] Update ru_RU, thanks to : rocket
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16710 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Добывить в коллекцию';
+$lang['Clear collection'] = 'Очистить';
+$lang['Collection name:'] = 'Название коллекции:';
+$lang['Collection'] = 'Коллекция';
+$lang['Collections'] = 'Коллекции';
+$lang['Copied'] = 'Скопировано';
+$lang['Copy to clipboard'] = 'Скопировано в буфер обмена';
+$lang['Create a new collection'] = 'Создать новую коллекцию';
+$lang['Invalid collection'] = 'Неверная коллекция';
+$lang['Please give a name'] = 'Пожалуйста, задайте имя';
+$lang['Public collection'] = 'Публичная';
+$lang['Remove from collection'] = 'Удалить из коллекции';
+$lang['Return to collections list'] = 'Вернутся к списку';
+$lang['Saved collections'] = 'Сохраненные коллекции';
+$lang['Save'] = 'Сохранить';
+$lang['See all my collections'] = 'Смотреть все мои коллекции';
+$lang['This collection is empty'] = 'Эта коллекция пуста';
+$lang['Unsaved collections'] = 'Несохраненные коллекции';
+$lang['You have %d collection'] = 'У вас %d коллекции';
+$lang['You have %d collections'] = 'У вас %d коллекции';
+$lang['You have no collection'] = 'У вас нет коллекций';
+$lang['active'] = 'активная';
+$lang['save'] = 'сохранить';
+$lang['set active'] = 'сделать активной';
+?>
