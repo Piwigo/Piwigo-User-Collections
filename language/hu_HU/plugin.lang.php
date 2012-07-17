1efd92dfb7254b5ad448dc727346ebfe08ec54fd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-17 07:08:09 +0000

    [user_collections] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16797 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add to collection'] = 'Gyűjteménybe';
+$lang['Clear collection'] = 'Kiürítés';
+$lang['Collection name:'] = 'Gyűjtemény neve:';
+$lang['Collection'] = 'Gyűjtemény';
+$lang['Collections'] = 'Gyűjtemények';
+$lang['Copied'] = 'Másolva';
+$lang['Copy to clipboard'] = 'Másolás a vágólapra';
+$lang['Create a new collection'] = 'Új gyűjtemény összeállítása';
+$lang['Invalid collection'] = 'Nem létező gyűjtemény';
+$lang['Public collection'] = 'Nyilvános';
+$lang['Remove from collection'] = 'Eltávolítás';
+$lang['Return to collections list'] = 'Vissza a gyűjtemény listához';
+$lang['Saved collections'] = 'Mentett gyűjtemény';
+$lang['Save'] = 'Mentés';
+$lang['This collection is empty'] = 'Ez a gyűjtemény még üres';
+$lang['Unsaved collections'] = 'Nem mentett gyűjtemény';
+$lang['You have %d collection'] = 'Önnek %d gyűjteménye van';
+$lang['You have %d collections'] = 'Önnek %d gyűjteménye van';
+$lang['You have no collection'] = 'Nincsenek gyűjtemények';
+$lang['active'] = 'aktív';
+$lang['by %s'] = 'összeállította: %s';
+$lang['set active'] = 'Aktiválás';
+?>
