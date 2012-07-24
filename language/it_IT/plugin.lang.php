6ca1288669e033578174647ac79fb820ce4e8c4f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-24 09:23:27 +0000

    [user_collections] Update it_IT, thanks to : Sugar888
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@16963 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d more...'] = '%d di più...';
+$lang['Add to collection'] = 'Aggiungi ad una raccolta';
+$lang['Clear collection'] = 'Cancella';
+$lang['Collection name:'] = 'Nome della collezione:';
+$lang['Collection'] = 'Collezione';
+$lang['Collections'] = 'collezioni';
+$lang['Copied'] = 'copiato';
+$lang['Copy to clipboard'] = 'Copia negli appunti';
+$lang['Create a new collection'] = 'Creare una nuova collezione';
+$lang['Invalid collection'] = 'collezione invalida';
+$lang['Please give a name'] = 'Si prega di dare un nome';
+$lang['Public collection'] = 'pubblico';
+$lang['Remove from collection'] = 'Eliminare dalla collezione';
+$lang['Return to collections list'] = 'ritornare alla lista';
+$lang['Saved collections'] = 'collezioni salvate';
+$lang['Save'] = 'salvare';
+$lang['See all my collections'] = 'Vedere tutte le proprie collezioni';
+$lang['This collection is empty'] = 'Questa collezione è vuota';
+$lang['Unsaved collections'] = 'collezioni non salvate';
+$lang['You have %d collection'] = 'Avete %d collezione';
+$lang['You have %d collections'] = 'Avete %d collezioni';
+$lang['You have no collection'] = 'Non avete nessuna collezione';
+$lang['active'] = 'attivo';
+$lang['by %s'] = 'da %s';
+$lang['save'] = 'salvare';
+$lang['set active'] = 'set attivo';
+?>
