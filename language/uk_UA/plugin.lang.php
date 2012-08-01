ca23ef2e2a00cc4bc626dcf360e9d3abc5e61991
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 15:41:40 +0000

    [user_collections] Update uk_UA, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@17284 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['by %s'] = 'від %s';
+$lang['%d more...'] = '%d більше...';
+$lang['You have %d collection'] = 'Ви маєте %d колекцію';
+$lang['You have %d collections'] = 'Ви маєте %d колекції(й)';
+$lang['You have no collection'] = 'У вас немає колекції';
+$lang['active'] = 'активний';
+$lang['save'] = 'зберегти';
+$lang['set active'] = 'встановити активним';
+$lang['Add to collection'] = 'Додати до колекції';
+$lang['Clear collection'] = 'Очистити';
+$lang['Collection'] = 'Колекція';
+$lang['Collection name:'] = 'Ім\'я колекції:';
+$lang['Collections'] = 'Колекції';
+$lang['Copied'] = 'Копіювання';
+$lang['Copy to clipboard'] = 'Копіювати до буфера обміну';
+$lang['Create a new collection'] = 'Створити нову колекцію';
+$lang['Invalid collection'] = 'Невірна колекція';
+$lang['Please give a name'] = 'Будь ласка, дайте назву';
+$lang['Public collection'] = 'Публічна';
+$lang['Remove from collection'] = 'Видалити з колекції';
+$lang['Return to collections list'] = 'Повернутися до списку';
+$lang['Save'] = 'Зберегти';
+$lang['Saved collections'] = 'Збережені колекції';
+$lang['See all my collections'] = 'Переглянути всі мої колекції';
+$lang['This collection is empty'] = 'Ця колекція порожня';
+$lang['Unsaved collections'] = 'Незбережені колекції';
+?>
