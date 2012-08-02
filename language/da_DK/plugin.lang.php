b277509b8520d64beaca0a40ac92c73bbd9dda75
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-02 07:10:45 +0000

    [user_collections] Update da_DK, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@17304 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You have %d collection'] = 'Du har %d samling';
+$lang['You have %d collections'] = 'Du har %d samlinger';
+$lang['You have no collection'] = 'Du har ingen samlinger';
+$lang['active'] = 'aktiv';
+$lang['by %s'] = 'til %s';
+$lang['save'] = 'gem';
+$lang['set active'] = 'set til aktiv';
+$lang['%d more...'] = '%d mere...';
+$lang['Add to collection'] = 'Tilføj til samling';
+$lang['Clear collection'] = 'Ryd';
+$lang['Collection'] = 'Samling';
+$lang['Collection name:'] = 'Navn på samling:';
+$lang['Collections'] = 'Samlinger';
+$lang['Copied'] = 'Kopieret';
+$lang['Copy to clipboard'] = 'Kopieret til klippebord';
+$lang['Create a new collection'] = 'Opret en ny samling';
+$lang['Invalid collection'] = 'Ugyldig samling';
+$lang['Please give a name'] = 'Angiv et navn';
+$lang['Public collection'] = 'Offentlig';
+$lang['Remove from collection'] = 'Fjern fra samling';
+$lang['Return to collections list'] = 'Tilbage til listen';
+$lang['Save'] = 'Gem';
+$lang['Saved collections'] = 'Gemte samlinger';
+$lang['See all my collections'] = 'Vis alle mine samlinger';
+$lang['This collection is empty'] = 'Denne samling er tom';
+$lang['Unsaved collections'] = 'Ikke gemte samlinger';
+?>
