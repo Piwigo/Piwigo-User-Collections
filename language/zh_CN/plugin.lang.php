045dae80ba977af015635fd693b2f7bfae172aa1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-30 04:54:37 +0000

    [user_collections] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@17664 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['by %s'] = 'by %s';
+$lang['save'] = '保存';
+$lang['active'] = '已激活';
+$lang['set active'] = '设置激活';
+$lang['You have no collection'] = '您没有精选集';
+$lang['You have %d collections'] = '您有 %d 个精选集';
+$lang['You have %d collection'] = '您有 %d 个精选集';
+$lang['Unsaved collections'] = '未保存的精选集';
+$lang['See all my collections'] = '查看所有精选集';
+$lang['This collection is empty'] = '此精选集为空';
+$lang['Saved collections'] = '已保存的精选集';
+$lang['Save'] = '保存';
+$lang['Return to collections list'] = '回到列表';
+$lang['Remove from collection'] = '从精选集中删除';
+$lang['Public collection'] = '公开';
+$lang['Please give a name'] = '请输入名称';
+$lang['Invalid collection'] = '无效的精选集';
+$lang['Create a new collection'] = '创建新的精选集';
+$lang['Copy to clipboard'] = '复制到剪贴板';
+$lang['Copied'] = '已复制';
+$lang['Collections'] = '精选集';
+$lang['Collection name:'] = '精选集名称：';
+$lang['Collection'] = '精选集';
+$lang['Clear collection'] = '清除';
+$lang['Add to collection'] = '添加到精选集';
+$lang['%d more...'] = '%d 更多...';
+?>
