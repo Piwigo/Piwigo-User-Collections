b78166ca5ddb819511648675719846be30cde0af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-02 21:26:29 +0000

    [user_collections] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@17696 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d more...'] = '%d بیشتر ...';
+$lang['Add to collection'] = 'افزودن به مجموعه';
+$lang['Clear collection'] = 'پاک کردن';
+$lang['Collection'] = 'مجموعه';
+$lang['Collection name:'] = 'نام مجموعه:';
+$lang['Collections'] = 'مجموعه ها';
+$lang['Copied'] = 'کپی شد';
+$lang['Copy to clipboard'] = 'کپی کردن';
+$lang['Create a new collection'] = 'ایجاد یک مجموعه ی تازه';
+$lang['Invalid collection'] = 'مجموعه ی نامعتبر';
+$lang['Please give a name'] = 'یک نام انتخاب کنید';
+$lang['Public collection'] = 'همگانی';
+$lang['Remove from collection'] = 'پاک کردن از مجموعه';
+$lang['Return to collections list'] = 'بازگشت به لیست';
+$lang['Save'] = 'ذخیره';
+$lang['Saved collections'] = 'مجموعه های ذخیره شده';
+$lang['See all my collections'] = 'مشاهده ی همه ی مجموعه های من';
+$lang['This collection is empty'] = 'این مجموعه خالی می باشد';
+$lang['Unsaved collections'] = 'مجموعه های ذخیره نشده';
+$lang['You have %d collection'] = 'شما %d مجموعه دارید';
+$lang['You have %d collections'] = 'شما %d مجموعه دارید';
+$lang['You have no collection'] = 'شما هیچ مجموعه ای ندارید';
+$lang['active'] = 'فعال';
+$lang['by %s'] = 'بوسیله ی %s';
+$lang['save'] = 'ذخیره';
+$lang['set active'] = 'فعال کردن';
+?>
