68e02bba284fce17f7e62cbc1cecb7f2b97a605c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-01 11:08:28 +0000

    [user_collections] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@18857 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You have %d collections'] = 'Teniu %d col·leccions';
+$lang['You have no collection'] = 'No teniu cap col·lecció';
+$lang['active'] = 'activa';
+$lang['by %s'] = 'per %s';
+$lang['save'] = 'desa';
+$lang['set active'] = 'Estableix com activa';
+$lang['%d more...'] = '%d més...';
+$lang['Add to collection'] = 'Afegeix a la col·lecció';
+$lang['Clear collection'] = 'Esborra';
+$lang['Collection'] = 'Col·lecció';
+$lang['Collection name:'] = 'Nom de la col·lecció';
+$lang['Collections'] = 'Col·leccions';
+$lang['Copied'] = 'S\'ha copiat';
+$lang['Copy to clipboard'] = 'Copia al porta-retalls';
+$lang['Create a new collection'] = 'Crea una nova col·lecció';
+$lang['Invalid collection'] = 'Col·lecció invàlida';
+$lang['Please give a name'] = 'Cal donar un nom';
+$lang['Public collection'] = 'Pública';
+$lang['Remove from collection'] = 'Elimina de la col·lecció';
+$lang['Return to collections list'] = 'Retorna a la llista';
+$lang['Save'] = 'Desa';
+$lang['Saved collections'] = 'Col·leccions desades';
+$lang['See all my collections'] = 'Visualitzar totes les meves col·leccions';
+$lang['This collection is empty'] = 'Aquesta col·lecció és buida';
+$lang['Unsaved collections'] = 'Col·leccions no desades';
+$lang['You have %d collection'] = 'Teniu %d col·lecció';
+?>
