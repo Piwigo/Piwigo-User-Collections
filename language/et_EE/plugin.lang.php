f0491220cce7786597d0aa9d6c3151c8c6c5abe6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-13 18:55:36 +0000

    [user_collections] Update et_EE, thanks to : kristian
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@20749 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,79 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Save'] = 'Salvesta';
+$lang['Saved collections'] = 'Salvestatud seeriad';
+$lang['See all my collections'] = 'Näita kõik mu seeriad';
+$lang['Send this collection my mail'] = 'Saada antud fotoseeria minu meilile';
+$lang['The collection must be public in order to send it'] = 'Et fotoseeriat saata, peab see olema avalikustatud';
+$lang['This collection is empty'] = 'Fotoseeriast puuduvad pildid';
+$lang['Unsaved collections'] = 'Salvestamata seeriad';
+$lang['You have %d collection'] = 'Sul on %d fotoseeria';
+$lang['You have %d collections'] = 'Sul on %d fotoseeriat';
+$lang['You have no collection'] = 'Sul puuduvad fotoseeriad';
+$lang['Your e-mail'] = 'Sinu epost';
+$lang['Your name'] = 'Sinu nimi';
+$lang['active'] = 'nähtaval';
+$lang['by %s'] = 'koostas %s';
+$lang['save'] = 'salvesta';
+$lang['set active'] = 'märgi nähtavaks';
+$lang['Edit this collection'] = 'Toimeta fotoseeriat';
+$lang['Error while sending e-mail'] = 'Eposti saatmisel tekkis viga';
+$lang['Export image list'] = 'Ekspordi fotode nimekiri';
+$lang['Fields'] = 'Väljad';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Tere, %s! %s saatis sulle fotoseeria leheküljelt <a href="%s">%s</a>.';
+$lang['Inactive'] = 'Ei ole nähtaval';
+$lang['Invalid collection'] = 'Vigane fotoseeria';
+$lang['Message (optional)'] = 'Sõnum (valikuline)';
+$lang['Number of images'] = 'Fotode arv';
+$lang['Please enter the recipient e-mail'] = 'Saaja eposti aadress';
+$lang['Please enter the recipient name'] = 'Saaja nimi';
+$lang['Please enter your e-mail'] = 'Sinu eposti aadress';
+$lang['Please enter your name'] = 'Sinu nimi';
+$lang['Please give a name'] = 'Seeria nimi';
+$lang['Preview'] = 'Eelvaade';
+$lang['Public collection'] = 'Avalik';
+$lang['Recipient e-mail'] = 'Saaja epost';
+$lang['Recipient name'] = 'Saaja nimi';
+$lang['Remove from collection'] = 'Eemalda seeriast';
+$lang['Return to collections list'] = 'Tagasi nimekirjale';
+$lang['%d more...'] = 'veel %d...';
+$lang['A photo collection by %s'] = '%s fotoseeria';
+$lang['Active'] = 'Nähtaval';
+$lang['Add to collection'] = 'Lisa fotoseeriasse';
+$lang['Allow users to send their public collections by mail'] = 'Luba kasutajatel teavitada avalikest fotoseeriatest meilitsi';
+$lang['Allow users to set their collections as public'] = 'Luba kasutajatel avalikustada oma fotoseeriaid';
+$lang['Clear'] = 'Tühista';
+$lang['Clear this collection'] = 'Tühjenda fotoseeria';
+$lang['Click here to view the complete collection'] = 'Näita tervet fotoseeriat';
+$lang['Collection'] = 'Fotoseeria';
+$lang['Collection name:'] = 'Fotoseeria nimi:';
+$lang['Collections'] = 'Fotoseeriad';
+$lang['Copied'] = 'Kopeeritud';
+$lang['Copy to clipboard'] = 'Kopeeri lõikepuhvrisse';
+$lang['Create a new collection'] = 'Loo uus fotoseeria';
+$lang['Date added to collection, new &rarr; old'] = 'Lisamiskuupäev, uued &rarr; vanad';
+$lang['Date added to collection, old &rarr; new'] = 'Lisamiskuupäev, vanad &rarr; uued';
+$lang['Delete this collection'] = 'Eemalda fotoseeria';
+$lang['Download CSV file'] = 'Laadi alla CSV-loendina';
+$lang['E-mail sent successfully'] = 'Epost saadetud';
+?>
