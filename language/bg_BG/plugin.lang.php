4c8d0cb6fadb87b28dce6a3e4af304f8c0860b77
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-19 10:25:51 +0000

    [user_collections] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@20839 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Unsaved collections'] = 'Незаписани колекции';
+$lang['Return to collections list'] = 'Връщате в списъка';
+$lang['You have no collection'] = 'Нямате колекции';
+$lang['Your e-mail'] = 'Вашият имейл';
+$lang['active'] = 'активно';
+$lang['Your name'] = 'Вашето име';
+$lang['Send this collection my mail'] = 'Изпрати тези колекция до имейла ми';
+$lang['See all my collections'] = 'Виж всички мой колекции';
+$lang['Saved collections'] = 'Запис на колекциите';
+$lang['Save'] = 'Запис';
+$lang['Public collection'] = 'Публично';
+$lang['Preview'] = 'Преглед';
+$lang['Number of images'] = 'Брой изображения';
+$lang['Message (optional)'] = 'Съобщение (опция)';
+$lang['Inactive'] = 'Неактивно';
+$lang['Invalid collection'] = 'Невалидна колекция';
+$lang['Fields'] = 'Полета';
+$lang['Export image list'] = 'Списък снимки за експорт';
+$lang['Error while sending e-mail'] = 'Грешка при пращане на имейл';
+$lang['Edit this collection'] = 'Редакция на колекцията';
+$lang['E-mail sent successfully'] = 'Успешно изпратен имейл';
+$lang['Download CSV file'] = 'Сваляне на CSV файл';
+$lang['Delete this collection'] = 'Изтриване на колекцията';
+$lang['Date added to collection, old &rarr; new'] = 'Дата добавена на колекция, стара &rarr; нова';
+$lang['Clear this collection'] = 'Изчистване на колекцията';
+$lang['Date added to collection, new &rarr; old'] = ' ';
+$lang['Create a new collection'] = 'Създаване нова колекция';
+$lang['Copy to clipboard'] = 'Копиране в клиборда';
+$lang['Copied'] = 'Копирано';
+$lang['Collections'] = 'Колекции';
+$lang['Collection name:'] = 'Име на колекция';
+$lang['Click here to view the complete collection'] = 'Натисни за преглед на завършената колекция';
+$lang['Collection'] = 'Колекция';
+$lang['Clear'] = 'Изчистване';
+$lang['Allow users to set their collections as public'] = 'Разрешение на потребителите за маркират като публични колекциите си';
+$lang['Allow users to send their public collections by mail'] = 'Разрешение на потребителите да изпращат публични колекции по имейл';
+$lang['Add to collection'] = 'Добавяне към колекция';
+$lang['Active'] = 'Активно';
+$lang['A photo collection by %s'] = 'Колекция снимки от %s';
+$lang['%d more...'] = '%d още...';
+?>
