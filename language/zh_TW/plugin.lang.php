88530010aec38723d1944ac66d1d9bd5a71e644c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-29 19:32:44 +0000

    [user_collections] Update zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@21908 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Error while sending e-mail'] = '發送電子郵件時發生錯誤';
+$lang['Export image list'] = '匯出圖片清單';
+$lang['Fields'] = '欄位';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = '您好 <b>%s</b>, 從<a href="%s">%s</a> 發送你 %s 的照片收藏集';
+$lang['Inactive'] = '待用';
+$lang['Invalid collection'] = '無效的收藏集';
+$lang['Message (optional)'] = '留言(可選)';
+$lang['Number of images'] = '圖片的數量';
+$lang['Please enter the recipient e-mail'] = '請輸入收件人的電子郵件';
+$lang['Please enter the recipient name'] = '請輸入收件人的姓名';
+$lang['Please enter your e-mail'] = '請輸入您的電子郵件信箱';
+$lang['Please enter your name'] = '請輸入您的姓名';
+$lang['Please give a name'] = '請提供一個名稱';
+$lang['Preview'] = '預覽';
+$lang['Public collection'] = '公共';
+$lang['Recipient e-mail'] = '收件人電子郵件';
+$lang['Recipient name'] = '收件人名稱';
+$lang['Remove from collection'] = '從收藏集中刪除';
+$lang['Return to collections list'] = '返回至名單';
+$lang['Edit this collection'] = '編輯本收藏集';
+$lang['Clear this collection'] = '清除此收藏集';
+$lang['Click here to view the complete collection'] = '點擊這裡查看完整的收藏集';
+$lang['Collection'] = '收藏集';
+$lang['Collection name:'] = '收藏集名稱：';
+$lang['Collections'] = '名稱：';
+$lang['Copied'] = '已複製';
+$lang['Copy to clipboard'] = '複製到剪貼板';
+$lang['Create a new collection'] = '建立新的收藏集';
+$lang['Date added to collection, new &rarr; old'] = '加入日期收藏集，新 &rarr; 舊';
+$lang['Date added to collection, old &rarr; new'] = '加入日期收藏集，舊 &rarr; 新';
+$lang['Delete this collection'] = '刪除此收藏集';
+$lang['Download CSV file'] = '下載CSV檔案';
+$lang['E-mail sent successfully'] = 'E-mail發送成功';
+$lang['%d more...'] = '%d 更多...';
+$lang['A photo collection by %s'] = '%s 的照片集';
+$lang['Active'] = '活躍';
+$lang['Add to collection'] = '新增至收藏集';
+$lang['Allow users to send their public collections by mail'] = '允許使用者通過郵件發送給他們的公開收藏';
+$lang['Allow users to set their collections as public'] = '允許使用者設定他們的收藏品為公開';
+$lang['Clear'] = '清除';
+?>
