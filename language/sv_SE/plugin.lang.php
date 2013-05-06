10b14f5cb793bf3fa13efcd51162babcbbdf4569
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-06 18:26:58 +0000

    [user_collections] Add sv_SE, thanks to : stefan63
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@22512 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,79 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Save'] = 'Spara';
+$lang['Saved collections'] = 'Sparade sammlingar';
+$lang['See all my collections'] = 'Se alla mina sammlingar';
+$lang['Send this collection my mail'] = 'Skicka den här sammlingen till min mail';
+$lang['The collection must be public in order to send it'] = 'Sammlingen måste vara publik för att kunna skickas';
+$lang['This collection is empty'] = 'Den här sammlingen är tom';
+$lang['Unsaved collections'] = 'Sammlingar som inte sparats';
+$lang['You have %d collection'] = 'Du har %d sammling';
+$lang['You have %d collections'] = 'Du har %d sammlingar';
+$lang['You have no collection'] = 'Du har inga sammlingar';
+$lang['Your e-mail'] = 'Din e-mail';
+$lang['Your name'] = 'Ditt namn';
+$lang['active'] = 'Aktiv';
+$lang['by %s'] = 'av %s';
+$lang['save'] = 'spara';
+$lang['set active'] = 'ange som aktiv';
+$lang['Edit this collection'] = 'Redigera den här bildsammlingen';
+$lang['Error while sending e-mail'] = 'Det blev ett fel när mailet skickades';
+$lang['Export image list'] = 'Exportera bild lista';
+$lang['Fields'] = 'Fält';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Hej<b>%s</b>, %sskickade dig en bildsammling från<a href="%s">%s</a>';
+$lang['Inactive'] = 'Inaktiv';
+$lang['Invalid collection'] = 'Ogiltlig sammling';
+$lang['Message (optional)'] = 'Meddelande (Valfritt)';
+$lang['Number of images'] = 'Antal bilder';
+$lang['Please enter the recipient e-mail'] = 'Vänligen ange mottagarens e-mail';
+$lang['Please enter the recipient name'] = 'Vänligen ange mottagarens namn';
+$lang['Please enter your e-mail'] = 'Vänligen ange din e-mail';
+$lang['Please enter your name'] = 'Vänligen ange ditt namn';
+$lang['Please give a name'] = 'Vänligen ange namn';
+$lang['Preview'] = 'Förhandsvisning';
+$lang['Public collection'] = 'Publik';
+$lang['Recipient e-mail'] = 'Mottagar e-mail';
+$lang['Recipient name'] = 'Mottagar namn';
+$lang['Remove from collection'] = 'Ta bort från sammling';
+$lang['Return to collections list'] = 'Återgå till listan';
+$lang['%d more...'] = '%d mer...';
+$lang['A photo collection by %s'] = 'En bildsammling av %s ';
+$lang['Active'] = 'Aktiv';
+$lang['Add to collection'] = 'Lägg till bildsammling';
+$lang['Allow users to send their public collections by mail'] = 'Tillåt andvändare att skicka deras publika bildsammlingar via mail';
+$lang['Allow users to set their collections as public'] = 'Tillåt andvändare att ange sina bildsammlingar som publika';
+$lang['Clear'] = 'Rensa';
+$lang['Clear this collection'] = 'Rensa den här bildsammlingen';
+$lang['Click here to view the complete collection'] = 'Klicka här för att se fullständig bildsammling';
+$lang['Collection'] = 'bildsammling';
+$lang['Collection name:'] = 'Namn på bildsammling:';
+$lang['Collections'] = 'bildsammlingar';
+$lang['Copied'] = 'Kopierad';
+$lang['Copy to clipboard'] = 'Kopiera till urklip';
+$lang['Create a new collection'] = 'Skapa en ny bildsammling';
+$lang['Date added to collection, new &rarr; old'] = 'Datum tillag till bildsammling, ny &rarr; gammal';
+$lang['Date added to collection, old &rarr; new'] = 'Datum tillag till bildsammling, gammal &rarr; ny';
+$lang['Delete this collection'] = 'Ta bort den här bildsammlingen';
+$lang['Download CSV file'] = 'Ladda ned CSV fil';
+$lang['E-mail sent successfully'] = 'E-mail skickat ';
+?>
