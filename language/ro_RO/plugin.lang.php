591d865a30d549f08c648ad5144959fb10b29bd3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-08 11:28:35 +0000

    [user_collections] Add ro_RO, thanks to : smarmie
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@23875 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,79 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Copy to clipboard'] = 'Copiază în clipboard';
+$lang['Date added to collection, new &rarr; old'] = 'Data adăugării la colecție, nou &rarr; vechi';
+$lang['Date added to collection, old &rarr; new'] = 'Data adăugării la colecție, vechi &rarr; nou';
+$lang['Name, A &rarr; Z'] = 'Nume, A &rarr; Z';
+$lang['Name, Z &rarr; A'] = 'Nume, Z &rarr; A';
+$lang['Photos number, high &rarr; low'] = 'Numărul fotografiei, mare &rarr; mic';
+$lang['Photos number, low &rarr; high'] = 'Numărul fotografiei, mic &rarr; mare';
+$lang['by %s'] = 'de %s';
+$lang['Allow users to set their collections as public'] = 'Permite utilizatorilor să-și stabilească colecțiile ca publice';
+$lang['Click here to view the complete collection'] = 'Apasă aici pentru a vedea colecția completă';
+$lang['Edit this collection'] = 'Modifică această colecție';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Salut, <b>%s</b>, %s ți-a trimis o colecție de fotografii de la <a href="%s">%s</a>';
+$lang['Please enter the recipient e-mail'] = 'Introdu adresa de e-mail a destinatarului';
+$lang['Please enter the recipient name'] = 'Introdu numele destinatarului';
+$lang['Please enter your e-mail'] = 'Introdu adresa ta de e-mail';
+$lang['Please enter your name'] = 'Introdu numele tău';
+$lang['Please give a name'] = 'Introdu un nume';
+$lang['You have %d collections'] = 'Ai %d colecții';
+$lang['You have no collection'] = 'Nu ai nicio colecție';
+$lang['Your e-mail'] = 'Adresa ta de e-mail';
+$lang['Your name'] = 'Numele tău';
+$lang['Send this collection my mail'] = 'Trimite-mi această colecție la adresa mea de e-mail';
+$lang['The collection must be public in order to send it'] = 'Această colecție trebuie să fie publică pentru a putea să o trimiți';
+$lang['This collection is empty'] = 'Această colecție este goală';
+$lang['You have %d collection'] = 'Ai %d colecții';
+$lang['Error while sending e-mail'] = 'Eroare de trimitere a e-mail-ului';
+$lang['Recipient name'] = 'Numele destinatarului';
+$lang['Return to collections list'] = 'Înapoi la listă';
+$lang['See all my collections'] = 'Vezi toate colecțiile mele';
+$lang['Save'] = 'Salvează';
+$lang['Remove from collection'] = 'Șterge din colecție';
+$lang['Preview'] = 'Previzualizare';
+$lang['Public collection'] = 'Public';
+$lang['Recipient e-mail'] = 'Destinatar e-mail';
+$lang['E-mail sent successfully'] = 'E-mail trimis cu succes';
+$lang['Export image list'] = 'Exportă lista de imagini';
+$lang['Fields'] = 'Câmpuri';
+$lang['Inactive'] = 'Inactiv';
+$lang['Invalid collection'] = 'Colecție invalidă';
+$lang['Message (optional)'] = 'Mesaj (opțional)';
+$lang['Number of images'] = 'Număr de imagini';
+$lang['%d more...'] = 'încă %d...';
+$lang['(remove)'] = '(șterge)';
+$lang['A photo collection by %s'] = 'O colecție de fotografii de %s';
+$lang['Active'] = 'Activ';
+$lang['Add to collection'] = 'Adaugă la colecție';
+$lang['Allow users to send their public collections by mail'] = 'Permite utilizatorilor să își trimită colecțiile publice prin e-mail';
+$lang['Clear'] = 'Șterge';
+$lang['Clear this collection'] = 'Curăță această colecție';
+$lang['Collection'] = 'Colecție';
+$lang['Collection name:'] = 'Numele colecției:';
+$lang['Collections'] = 'Colecții';
+$lang['Copied'] = 'Copiat';
+$lang['Create a new collection'] = 'Crează o colecție nouă';
+$lang['Delete this collection'] = 'Șterge această colecție';
+$lang['Download CSV file'] = 'Descarcă fișier CSV';
+?>
