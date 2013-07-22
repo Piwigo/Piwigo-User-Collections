871ce07e5cdd4b196b8cb0e046a54c1e95d39b59
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-22 09:38:20 +0000

    [user_collections] Add zh_HK, thanks to : gurunited
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@24015 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,79 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Date added to collection, new &rarr; old'] = '日期加入收藏集, 新 &rarr; 舊';
+$lang['Date added to collection, old &rarr; new'] = '日期加入收藏集, 舊 &rarr; 新';
+$lang['Fields'] = '項目';
+$lang['See all my collections'] = '看我的收藏集';
+$lang['Inactive'] = '停用';
+$lang['Invalid collection'] = '錯誤的收藏集';
+$lang['Message (optional)'] = '訊息(選擇性)';
+$lang['Export image list'] = '輸出圖片列表';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = '好你<b>%s</b>, %s 發給你一個相片收藏集由<a href="%s">%s</a>';
+$lang['Name, A &rarr; Z'] = '姓名, A &raa; Z';
+$lang['Name, Z &rarr; A'] = '姓名, Z &raa; A';
+$lang['Number of images'] = '圖片數目　';
+$lang['Photos number, high &rarr; low'] = '相片數目, 高 &rarr; 低';
+$lang['Photos number, low &rarr; high'] = '相片數目, 低 &rarr; 高';
+$lang['Please enter the recipient e-mail'] = '請輸入收件人電郵';
+$lang['Please enter the recipient name'] = '請輸入收件人名稱';
+$lang['Please enter your e-mail'] = '請輸入你的電郵';
+$lang['Please enter your name'] = '請輸入你的名稱';
+$lang['Please give a name'] = '請給一個名稱';
+$lang['Preview'] = '預覽';
+$lang['Public collection'] = '公開';
+$lang['Recipient e-mail'] = '收件人電郵';
+$lang['Recipient name'] = '收件人名稱';
+$lang['Remove from collection'] = '從收藏集刪除';
+$lang['Return to collections list'] = '返回列表';
+$lang['Save'] = '儲存';
+$lang['Send this collection my mail'] = '發送這個收藏集至我的電郵';
+$lang['The collection must be public in order to send it'] = '收藏集要設定為公開才可發送';
+$lang['This collection is empty'] = '這個收藏集是空的';
+$lang['You have %d collection'] = '有你　%d 收藏集';
+$lang['You have %d collections'] = '有你　%d 收藏集';
+$lang['You have no collection'] = '你沒有收藏集';
+$lang['Your e-mail'] = '你的電郵　';
+$lang['Your name'] = '你的名稱';
+$lang['by %s'] = '以 %s';
+$lang['Click here to view the complete collection'] = '接這裡觀看所有收藏集';
+$lang['Collection'] = '收藏集';
+$lang['Collection name:'] = '收藏集名稱';
+$lang['Collections'] = '收藏集';
+$lang['Copied'] = '抄錄';
+$lang['Copy to clipboard'] = '抄錄至剪貼簿';
+$lang['Create a new collection'] = '新增剪貼簿';
+$lang['Delete this collection'] = '刪除收藏集';
+$lang['Download CSV file'] = '下載csv檔案';
+$lang['E-mail sent successfully'] = '電郵發送成功';
+$lang['Edit this collection'] = '編輯這個收藏集';
+$lang['Error while sending e-mail'] = '發送電郵時發生錯誤';
+$lang['%d more...'] = '%d 更多';
+$lang['(remove)'] = '(刪除)';
+$lang['A photo collection by %s'] = '%s 的相片收藏集';
+$lang['Active'] = '激活';
+$lang['Add to collection'] = '增加至收藏集';
+$lang['Allow users to send their public collections by mail'] = '允許用戶以電郵發放公開的收藏集';
+$lang['Allow users to set their collections as public'] = '允許用戶設定收藏集為公開';
+$lang['Clear'] = '清空';
+$lang['Clear this collection'] = '清空這個收藏集';
+?>
