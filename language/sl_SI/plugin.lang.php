25113f09b6ba913ed259514091a574db0e885ee5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-14 10:25:13 +0000

    [user_collections] Add sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@24451 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d more...'] = '%in več';
+$lang['(remove)'] = '(odstrani)';
+$lang['A photo collection by %s'] = 'Zbirka fotografij uporabnika %';
+$lang['Active'] = 'Aktivno';
+$lang['Add to collection'] = 'Dodaj v zbirko';
+$lang['Allow users to send their public collections by mail'] = 'Dovoli uporabnikom pošiljanje javnih zbirk po e-pošti';
+$lang['Allow users to set their collections as public'] = 'Dovoli uporabnikom da nastavijo svoje zbirke kot javne';
+$lang['Clear'] = 'Počisti';
+$lang['Clear this collection'] = 'Počisti to zbirko';
+$lang['Click here to view the complete collection'] = 'Klikni tu za ogled celotne zbirke';
+$lang['Collection'] = 'Zbirka';
+$lang['Collection name:'] = 'Ime zbirke';
+$lang['Collection not found'] = 'Ne najdem te zbirke';
+$lang['Collections'] = 'Zbirke';
+$lang['Create a new collection'] = 'Ustvari novo zbirko';
+$lang['Date added to collection, new &rarr; old'] = 'Datum dodajanja v zbirko, nova &rarr; stara';
+$lang['Date added to collection, old &rarr; new'] = 'Datum dodajanja v zbirko, stara &rarr; nova';
+$lang['Delete this collection'] = 'Izbriši to zbirko';
+$lang['Download CSV file'] = 'Prenesi CSV datoteko';
+$lang['E-mail sent successfully'] = 'E-pošta uspešno poslana.';
+?>
