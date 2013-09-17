bbe7518515b4a0b08d03c1ddf9d09424a2c41205
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-17 05:30:28 +0000

    [user_collections] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@24490 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['A photo collection by %s'] = 'Fotokolekto de %s';
+$lang['Active'] = 'Aktivaj';
+$lang['Add to collection'] = 'Aldoni al la kolekto';
+$lang['Allow users to send their public collections by mail'] = 'Permesas al uzantoj sendi iliajn publikajn kolektojn rete';
+$lang['Allow users to set their collections as public'] = 'Permesas al uzantoj agordi iliajn kolektojn kiel publikajn';
+$lang['Clear'] = 'Vakigi';
+$lang['Clear this collection'] = 'Vakigi tiun kolekton';
+$lang['Click here to view the complete collection'] = 'Musklaku ĉitie por vidi la tutan kolekton';
+$lang['Collection'] = 'Kolekto';
+$lang['Collection name:'] = 'Kolektonomo:';
+$lang['Collection not found'] = 'Kolekto ne trovita';
+$lang['Collections'] = 'Kolektoj';
+$lang['Create a new collection'] = 'Krei novan kolekton';
+$lang['Date added to collection, new &rarr; old'] = 'Aldondato al kolekto, nova &rarr; malnova';
+$lang['Date added to collection, old &rarr; new'] = 'Aldondato al kolekto, malnova &rarr; nova';
+$lang['Delete this collection'] = 'Forigi la kolekton';
+$lang['Download CSV file'] = 'Elŝuti la CSV-dosieron';
+$lang['E-mail sent successfully'] = 'Retmesaĝo sukcese sendiĝis';
+$lang['%d more...'] = 'kaj %d pli...';
+$lang['(remove)'] = '(forigi)';
+?>
