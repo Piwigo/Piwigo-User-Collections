e03dae2ce055574d5c30af6b959b80b5bbc6bf62
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-19 09:42:18 +0000

    [user_collections] Update lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@24989 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,84 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['New share added: <a href="%s">%s</a>'] = 'Sukurta nauja prieiga: <a href="%s">%s</a>';
+$lang['by %s'] = 'pagal %s';
+$lang['Preview'] = 'Peržiūrėti';
+$lang['Recipient e-mail'] = 'Gavėjo e-mail';
+$lang['Recipient name'] = 'Gavėjo vardas';
+$lang['Remove from collection'] = 'Pašalinti iš kolekcijos';
+$lang['Restricted access'] = 'Apribotas priėjimas';
+$lang['Save'] = 'Išsaugoti';
+$lang['Send this collection my mail'] = 'Atsiųsti šią kolekciją man el. paštu';
+$lang['Share'] = 'Prieiga';
+$lang['Share deleted'] = 'Prieiga pašalinta';
+$lang['Share key'] = 'Prieigos raktas';
+$lang['Share this collection'] = 'Dalintis šia kolekcija';
+$lang['The key must be at least 8 characters long'] = 'Raktas turi būti mažiausiai iš 8 simbolių';
+$lang['This collection is empty'] = 'Ši kolekcija tuščia';
+$lang['This key is already used'] = 'Šis raktas jau naudojamas';
+$lang['This link expired'] = 'Nuoroda nebegalioja';
+$lang['You have %d collection'] = 'Jūs turite %d kolekciją';
+$lang['You have %d collections'] = 'Jūs turite %d kolekcijų';
+$lang['You have no collection'] = 'Jūs neturite kolekcijų';
+$lang['Your e-mail'] = 'Jūsų el-paštas';
+$lang['Your name'] = 'Jūsų vardas';
+$lang['Edit this collection'] = 'Redaguoti šią kolekciją';
+$lang['Error while sending e-mail'] = 'Nepavyko išsiųsti E-mail\'o';
+$lang['Expiration date'] = 'Galiojimo data';
+$lang['Export image list'] = 'Eksportuoti paveiksliukų sąrašą';
+$lang['Fields'] = 'Laukeliai';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Labas <b>%s</b>, %s išsiuntė Tau nuotraukų kolekciją iš <a href="%s">%s</a>';
+$lang['Inactive'] = 'Neaktyvi';
+$lang['Invalid collection'] = 'Bloga kolekcija';
+$lang['Message (optional)'] = 'Pranešimas (nebūtina)';
+$lang['Name, A &rarr; Z'] = 'Vardas, A &rarr; Z';
+$lang['Name, Z &rarr; A'] = 'Vardas, Z &rarr; A';
+$lang['Number of images'] = 'Paveiksliukų skaičius';
+$lang['Photos number, high &rarr; low'] = 'Nuotraukos nr. didžiausia &rarr; mažiausia';
+$lang['Photos number, low &rarr; high'] = 'Nuotraukos nr. mažiausia &rarr; didžiausia';
+$lang['Please enter the recipient e-mail'] = 'Įveskite gavėjo e-mail';
+$lang['Please enter the recipient name'] = 'Įveskite gavėjo vardą';
+$lang['Please enter your e-mail'] = 'Įveskite savo email';
+$lang['Please enter your name'] = 'Įveskite savo vardą';
+$lang['Please give a name'] = 'Suteikite pavadinimą';
+$lang['%d more...'] = '%d daugiau...';
+$lang['(remove)'] = '(pašalinti)';
+$lang['A photo collection by %s'] = '%s nuotraukų kolekcija';
+$lang['Active'] = 'Aktyvi';
+$lang['Add to collection'] = 'Pridėti į kolekciją';
+$lang['Allow users to send their public collections by mail'] = 'Leisti vartotojams atsisiųsti viešas kolekcijas paštu';
+$lang['Allow users to set their collections as public'] = 'Leisti vartotojams savo kolekcijas nustatyti kaip viešas';
+$lang['Clear'] = 'Išvalyti';
+$lang['Clear this collection'] = 'Išvalyti šią kolekcija';
+$lang['Click here to view the complete collection'] = 'Spustelėkite, norėdami pamatyti visą kolekciją';
+$lang['Collection'] = 'Kolekcija';
+$lang['Collection name:'] = 'Kolekcijos pavadinimas';
+$lang['Collection not found'] = 'Kolekcija nerasta';
+$lang['Collections'] = 'Kolekcijos';
+$lang['Create a new collection'] = 'Sukurti naują kolekciją';
+$lang['Date added to collection, new &rarr; old'] = 'Data, pridėta į kolekciją nauja &rarr; sena';
+$lang['Date added to collection, old &rarr; new'] = 'Data, pridėta į kolekciją sena &rarr; nauja';
+$lang['Delete this collection'] = 'Pašalinti šią kolekciją';
+$lang['Download CSV file'] = 'Atsisiųsti CSV failą';
+$lang['E-mail sent successfully'] = 'E-mail\'as išsiųstas sėkmingai';
+?>
