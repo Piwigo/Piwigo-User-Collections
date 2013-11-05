154fa9b02ba6a5a875977bd4f11addb33fba48f0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-05 19:33:38 +0000

    [user_collections] Update ar_SA, thanks to : aldambi
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@25349 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,84 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['by %s'] = 'بواسطة %s';
+$lang['Preview'] = 'معاينة';
+$lang['Recipient e-mail'] = 'بريد المستلم';
+$lang['Recipient name'] = 'اسم المستلم';
+$lang['Remove from collection'] = 'حذف من المجموعة';
+$lang['Restricted access'] = 'وصول مقيد';
+$lang['Save'] = 'حفظ';
+$lang['Send this collection my mail'] = 'ارسال هذه المجموعةالى بريدي الالكتروني';
+$lang['Share'] = 'مشاركة';
+$lang['Share deleted'] = 'المشاركة حذفت';
+$lang['Share key'] = 'مفتاح المشاركة';
+$lang['Share this collection'] = 'مشاركة هذه المجموعة';
+$lang['The key must be at least 8 characters long'] = 'يجب أن يتكون المفتاح من 8 أحرف على الأقل';
+$lang['This collection is empty'] = 'هذه المجموعة فارغة';
+$lang['This key is already used'] = 'هذا المفتاح مستخدم من قبل';
+$lang['This link expired'] = 'هذا الرابط منتهي الصلاحية';
+$lang['You have %d collection'] = 'لديك %d مجموعة';
+$lang['You have %d collections'] = 'لديك %d مجموعات';
+$lang['You have no collection'] = 'ليس لديك اي مجموعة';
+$lang['Your e-mail'] = 'بريدك الالكتروني';
+$lang['Your name'] = 'اسمك';
+$lang['Edit this collection'] = 'تحرير هذه المجموعة';
+$lang['Error while sending e-mail'] = 'حصل خطأ عند ارسال البريد';
+$lang['Expiration date'] = 'تاريخ انتهاء الصلاحية';
+$lang['Export image list'] = 'تصدير قائمة الصور';
+$lang['Fields'] = 'الحقول';
+$lang['Inactive'] = 'غير فعال';
+$lang['Invalid collection'] = 'مجموعة خاطئة';
+$lang['Number of images'] = 'ارقام الصور';
+$lang['Please enter the recipient name'] = 'يرجى إدخال اسم المستلم';
+$lang['Please enter your e-mail'] = 'يرجى ادخال بريدك الالكتروني';
+$lang['Please enter your name'] = 'يرجى ادخال اسمك';
+$lang['Please give a name'] = 'يرجى إعطاء اسم';
+$lang['Please enter the recipient e-mail'] = 'يرجى إدخال البريد الإلكتروني للمستلم';
+$lang['Photos number, low &rarr; high'] = 'عدد الصور، منخفض → عالي';
+$lang['Photos number, high &rarr; low'] = 'عدد الصور، عالي → منخفض ';
+$lang['New share added: <a href="%s">%s</a>'] = 'مشاركة جديدة اضيفت: <a href="%s">%s</a>';
+$lang['Name, A &rarr; Z'] = 'اسم، A → Z';
+$lang['Name, Z &rarr; A'] = 'اسم، Z → A';
+$lang['Message (optional)'] = 'رسالة (اختياري)';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'مرحبا<b>%s</b>, %s أرسل لك مجموعة صور من<a href="%s">%s</a>';
+$lang['Active'] = 'فعال';
+$lang['Create a new collection'] = 'انشاء مجموعة جديدة';
+$lang['Collection'] = 'مجموعة';
+$lang['E-mail sent successfully'] = 'إرسل البريد الإلكتروني بنجاح';
+$lang['Download CSV file'] = 'تحميل ملف CSV';
+$lang['Delete this collection'] = 'حذف هذه المجموعة';
+$lang['Date added to collection, old &rarr; new'] = 'اضيف التاريخ الى المجموعةالقديمة → الجديدة';
+$lang['Date added to collection, new &rarr; old'] = 'اضيف التاريخ الى المجموعة ، الجديدة القديمة →';
+$lang['Collections'] = 'مجموعات';
+$lang['Collection not found'] = 'المجموعة غير موجودة';
+$lang['Collection name:'] = 'اسم المجموعة:';
+$lang['Click here to view the complete collection'] = 'انقر هنا لعرض المجموعة الكاملة';
+$lang['Clear this collection'] = 'مسح هذه المجموعة';
+$lang['Clear'] = 'مسح';
+$lang['Allow users to set their collections as public'] = 'السماح للمستخدمين بضبط مجموعاتهم كعامة';
+$lang['Allow users to send their public collections by mail'] = 'السماح للمستخدمين بارسال ايميل للمجموعات العامة الخاصة بهم ';
+$lang['Add to collection'] = 'إضافة إلى المجموعة';
+$lang['A photo collection by %s'] = 'مجموعة صور من قبل %s';
+$lang['%d more...'] = '%d اكثر ...';
+$lang['(remove)'] = '(إزالة)';
+?>
