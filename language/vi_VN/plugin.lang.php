0855294068eabedc301765048a797dc86e4f7013
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-23 13:05:48 +0000

    [user_collections] Add vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@26122 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,91 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This link expired'] = 'Liên kết đã hết hạn';
+$lang['You have %d collection'] = 'Bạn có %d bộ sưu tập';
+$lang['You have %d collections'] = 'Bạn có %d bộ sưu tập';
+$lang['You have no collection'] = 'Bạn không có bộ sưu tập nào';
+$lang['Your e-mail'] = 'E-mail của bạn';
+$lang['Your name'] = 'Tên của bạn';
+$lang['by %s'] = 'của %s';
+$lang['jump to photo'] = 'nhảy đến hình ảnh';
+$lang['Please enter the recipient e-mail'] = 'Vui lòng nhập e-mail người nhận';
+$lang['Please enter the recipient name'] = 'Vui lòng nhập tên người nhận';
+$lang['Please enter your e-mail'] = 'Vui lòng nhập e-mail của bạn';
+$lang['Please enter your name'] = 'Vui lòng nhập tên bạn';
+$lang['Please give a name'] = 'Vui lòng nhập một tên';
+$lang['Preview'] = 'Xem qua';
+$lang['Recipient e-mail'] = 'E-mail người nhận';
+$lang['Recipient name'] = 'Tên người nhận';
+$lang['Remove from collection'] = 'Xóa khỏi bộ sưu tập';
+$lang['Restricted access'] = 'Truy xuất bị giới hạn';
+$lang['Save'] = 'Lưu';
+$lang['Send'] = 'Gửi';
+$lang['Send this collection by mail'] = 'Gửi bộ sưu tập qua mail';
+$lang['Share'] = 'Chia sẻ';
+$lang['Share deleted'] = 'Chia sẻ đã bị xóa';
+$lang['Share key'] = 'Chia sẻ khóa';
+$lang['Share this collection'] = 'Chia sẻ bộ sưu tập này';
+$lang['The key must be at least 8 characters long'] = 'Khóa phải dài ít nhất 8 ký tự';
+$lang['This collection is empty'] = 'Bộ sưu tập này rỗng';
+$lang['This key is already used'] = 'Khóa đã được dùng';
+$lang['Description'] = 'Mô tả';
+$lang['Download CSV file'] = 'Tải file CSV';
+$lang['E-mail sent successfully'] = 'E-mail được gửi thành công';
+$lang['Edit this collection'] = 'Sửa bộ sưu tập này';
+$lang['Error while sending e-mail'] = 'Lỗi khi gửi e-mail';
+$lang['Expiration date'] = 'Ngày tháng quá hạn';
+$lang['Export image list'] = 'Xuất danh sách ảnh';
+$lang['Fields'] = 'Trường';
+$lang['Hello %s, %s sent you a photos collection from "%s"'] = 'Xin chào %s, %s gửi bạn một bộ sưu tập ảnh từ "%s"';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Xin chào <b>%s</b>, %s gửi bạn một bộ sưu tập ảnh từ <a href="%s">%s</a>';
+$lang['Inactive'] = 'Không kích hoạt';
+$lang['Invalid collection'] = 'Bộ sưu tập không hợp lệ';
+$lang['Message (optional)'] = 'Tin nhắn (tùy chọn)';
+$lang['Name'] = 'Tên';
+$lang['Name, A &rarr; Z'] = 'Tên, A &rarr; Z';
+$lang['Name, Z &rarr; A'] = 'Tên, Z &rarr; A';
+$lang['New share added: <a href="%s">%s</a>'] = 'Chia sẻ mới được thêm vào: <a href="%s">%s</a>';
+$lang['Number of images'] = 'Số lượng ảnh';
+$lang['Photos number, high &rarr; low'] = 'Số ảnh,  cao &rarr; thấp';
+$lang['Photos number, low &rarr; high'] = 'Số ảnh,  thấp &rarr; cao';
+$lang['%d more...'] = '%d ảnh thêm...';
+$lang['(remove)'] = '(xóa)';
+$lang['A photo collection by %s'] = 'Một bộ sưu tập ảnh của %s';
+$lang['Active'] = 'Kích hoạt';
+$lang['Add'] = 'Thêm';
+$lang['Add to collection'] = 'Thêm vào bộ sưu tập';
+$lang['Allow users to send their public collections by mail'] = 'Cho phép người dùng gửi các bộ sưu tập ở chế độ qua mail';
+$lang['Allow users to set their collections as public'] = 'Cho phép người dùng đặt các bộ sưu tập ở chế độ công cộng';
+$lang['Cancel'] = 'Hủy';
+$lang['Clear'] = 'Xóa';
+$lang['Clear this collection'] = 'Xóa bộ sưu tập này';
+$lang['Click here to view the complete collection'] = 'Ấn vào đây để xem toàn bộ bộ sưu tập';
+$lang['Collection'] = 'Bộ sưu tập';
+$lang['Collection name:'] = 'Tên bộ sưu tập:';
+$lang['Collection not found'] = 'Không tìm thấy bộ sưu tập';
+$lang['Collections'] = 'Bộ sưu tập';
+$lang['Create a new collection'] = 'Tạo bộ sưu tập mới';
+$lang['Date added to collection, new &rarr; old'] = 'Ngày tháng được thêm vào bộ sưu tập, new &rarr; old';
+$lang['Date added to collection, old &rarr; new'] = 'Ngày tháng được thêm vào bộ sưu tập, old &rarr; new';
+$lang['Delete this collection'] = 'Xóa bộ sưu tập này';
+?>
