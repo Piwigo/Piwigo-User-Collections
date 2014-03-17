19611d9faae5586c79962b0baf2ff79f84afdc81
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-17 17:18:41 +0000

    [user_collections] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@27806 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/mn_MN/plugin.lang.php
@@ -0,0 +1,76 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The key must be at least 8 characters long'] = 'Түлхүүр үг доод тал нь 8 тэмдэгт байх ёстой';
+$lang['This key is already used'] = 'Уучлаарай, ийм нэр ашиглагдаж байна';
+$lang['Click here to view the complete collection'] = 'Цуглуулгаа харахыг хүсвэл энд дарна уу';
+$lang['Collection not found'] = 'Ийт цуглуулга олдсонгүй';
+$lang['Message (optional)'] = 'Зурвас (шаардлагатай бус)';
+$lang['Photos number, high &rarr; low'] = 'Зургийн тоогоор, ихээс &rarr; бага';
+$lang['Photos number, low &rarr; high'] = 'Зургийн тоогоор, багаас &rarr; их';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = '<b>%s</b> Та сайн байна уу, <a href="%s">%s</a>-н хэрэглэгч %s танд  зургийн цуглуулга илгээсэн байна';
+$lang['Inactive'] = 'Идэвхгүй';
+$lang['Please give a name'] = 'Нэр өгнө үү';
+$lang['Download CSV file'] = 'CSV файлыг татах';
+$lang['Send this collection by mail'] = 'Тухайн цуглуулгийг и-мэйл-р илгээх';
+$lang['Please enter the recipient name'] = 'Хүлээн авагчийн нэрээ оруулна уу';
+$lang['Please enter your name'] = 'Өөрийнхөө нэрийг оруулна уу';
+$lang['This collection is empty'] = 'Тухайн цуглуулга хоосон байна';
+$lang['Recipient name'] = 'Хүлээн авагчийн нэр';
+$lang['(remove)'] = '(хасах)';
+$lang['A photo collection by %s'] = '%s-н зургийн цуглуулга';
+$lang['Please enter the recipient e-mail'] = 'Хүлээн авагчийн и-мэйл хаягаа оруулна уу';
+$lang['Delete this collection'] = 'Тухайн цуглуулгийг устгах';
+$lang['Description'] = 'Тайлбар';
+$lang['E-mail sent successfully'] = 'И-мэйлийг амжилттай илгээлээ';
+$lang['Edit this collection'] = 'Тухайн цуглуулгийг засах';
+$lang['Error while sending e-mail'] = 'И-мэйлийг илгээхэд алдаа гарлаа';
+$lang['Expiration date'] = 'Дуусах хугацаа';
+$lang['Fields'] = 'Талбар';
+$lang['Hello %s, %s sent you a photos collection from "%s"'] = 'Сайн байна уу %s, %s танд "%s"-с зургийн цуглуулга илгээсэн байна';
+$lang['This link expired'] = 'Тухайн холбоос хуучирсан байна';
+$lang['You have %d collection'] = 'Танд %d цуглуулга байна';
+$lang['You have %d collections'] = 'Танд %d цуглуулга байна';
+$lang['You have no collection'] = 'Танд цуглуулга байхгүй байна';
+$lang['Your e-mail'] = 'Таны и-мэйл';
+$lang['Your name'] = 'Таны нэр';
+$lang['jump to photo'] = 'зураг уруу очих';
+$lang['Save'] = 'Хадгалах';
+$lang['Send'] = 'Илгээх';
+$lang['Name'] = 'Нэр';
+$lang['Name, A &rarr; Z'] = 'Нэр, A &rarr; Z';
+$lang['Name, Z &rarr; A'] = 'Нэр, Z &rarr; A';
+$lang['Number of images'] = 'Зургийн тоо';
+$lang['Please enter your e-mail'] = 'и-мэйл хаягаа оруулна уу';
+$lang['Preview'] = 'Урьдчилан харах';
+$lang['Recipient e-mail'] = 'Хүлээн авагчийн и-мэйл';
+$lang['Remove from collection'] = 'Цуглуулгаас хасах';
+$lang['Active'] = 'Идэвхтэй';
+$lang['Add'] = 'Нэмэх';
+$lang['Add to collection'] = 'Цуглуулганд нэмэх';
+$lang['Cancel'] = 'Цуцлах';
+$lang['Clear'] = 'Цэвэрлэх';
+$lang['Collection'] = 'Цуглуулга';
+$lang['Collection name:'] = 'Цуглуулгын нэр:';
+$lang['Collections'] = 'Цуглуулга';
+$lang['Create a new collection'] = 'Шинэ цуглуулга үүсгэх';
+?>
