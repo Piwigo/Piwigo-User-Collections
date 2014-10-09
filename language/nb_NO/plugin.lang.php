dc7aa3f3c110ce4a0899a36c144865ccf2bf6911
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-09 19:41:42 +0000

    [user_collections] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/UserCollections@29954 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,94 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The key must be at least 8 characters long'] = 'Nøkkelen må være minst 8 tegn lang';
+$lang['This collection is empty'] = 'Denne samlingen er tom';
+$lang['This key is already used'] = 'Denne nøkkelen er allerede i bruk';
+$lang['This link expired'] = 'Denne koblingen er utløpt';
+$lang['To'] = 'Til';
+$lang['You have %d collection'] = 'Du har %d samling';
+$lang['You have %d collections'] = 'Du har %d samling';
+$lang['You have no collection'] = 'Du har ingen samling';
+$lang['Your e-mail'] = 'Den e-post adresse';
+$lang['Your name'] = 'Ditt navn';
+$lang['by %s'] = 'av %s';
+$lang['jump to photo'] = 'hoppe til bilde';
+$lang['Photos number, high &rarr; low'] = 'Bilde nummer, høy & rarr; lav';
+$lang['Photos number, low &rarr; high'] = 'Bilder nummer, lav & rarr; høy';
+$lang['Please enter the recipient e-mail'] = 'Skriv inn mottakers e-post';
+$lang['Please enter the recipient name'] = 'Skriv inn mottakers navn';
+$lang['Please enter your e-mail'] = 'Skriv inn din e-post';
+$lang['Please enter your name'] = 'Skriv inn ditt navn';
+$lang['Please give a name'] = 'Vennligst oppgi et navn';
+$lang['Preview'] = 'Forhåndsvisning';
+$lang['Recipient e-mail'] = 'Mottakers e-post';
+$lang['Recipient name'] = 'Mottakers navn';
+$lang['Remove from collection'] = 'Fjern fra samlingen';
+$lang['Restricted access'] = 'Begrenset adgang';
+$lang['Save'] = 'Lagre';
+$lang['Send'] = 'Send';
+$lang['Send this collection by mail'] = 'Send denne samlingen med e-post';
+$lang['Share'] = 'Del';
+$lang['Share deleted'] = 'Del er slettet';
+$lang['Share key'] = 'Del-tasten';
+$lang['Share this collection'] = 'Del denne samlingen';
+$lang['Someone else'] = 'Noen andre';
+$lang['Date added to collection, old &rarr; new'] = 'Dato lagt til samlingen, gammel &rarr; ny';
+$lang['Delete this collection'] = 'Slett denne samlingen';
+$lang['Description'] = 'Beskrivelse';
+$lang['Download CSV file'] = 'Last ned CSV fil';
+$lang['E-mail sent successfully'] = 'E-post sendt korrekt';
+$lang['Edit this collection'] = 'Rediger denne samlingen';
+$lang['Error while sending e-mail'] = 'Feil ved sending av e-post';
+$lang['Expiration date'] = 'Utløpsdato';
+$lang['Export image list'] = 'Eksporter bildeliste';
+$lang['Fields'] = 'Felt';
+$lang['Hello %s, %s sent you a photos collection from "%s"'] = 'Hei%s,%s har sendt deg en bilde samling fra "%s"';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Hei <b>%s</b>, %s har sendt deg en bilde samling fra <a href="%s">%s</a>';
+$lang['Inactive'] = 'Inaktiv';
+$lang['Invalid collection'] = 'Ugyldig samling';
+$lang['Message (optional)'] = 'Melding (valgfritt)';
+$lang['Name'] = 'Navn';
+$lang['Name, A &rarr; Z'] = 'Navn, A &rarr; Z';
+$lang['Name, Z &rarr; A'] = 'Navn, Z &rarr; A';
+$lang['New share added: <a href="%s">%s</a>'] = 'Ny deling lagt til:<a href="%s">%s</a>';
+$lang['Number of images'] = 'Antall bilder';
+$lang['%d more...'] = '%d mere...';
+$lang['(remove)'] = '(fjern)';
+$lang['A photo collection by %s'] = 'En bildeserie av%s';
+$lang['Active'] = 'Aktiv';
+$lang['Add'] = 'Legg til';
+$lang['Add to collection'] = 'Legg til samlingen';
+$lang['Administrator'] = 'Administrator';
+$lang['Allow users to send their collections (public or not) to the admin'] = 'Tillat brukere å sende sine samlinger (offentlige eller ikke) til admin';
+$lang['Allow users to send their public collections by mail'] = 'Tillat brukere å sende sine offentlige samlinger via e-post';
+$lang['Allow users to set their collections as public'] = 'Tillat brukere å sette sine samlinger som offentlig';
+$lang['Cancel'] = 'Avbryt';
+$lang['Clear'] = 'Fjern';
+$lang['Clear this collection'] = 'Fjerne denne samlingen';
+$lang['Click here to view the complete collection'] = 'Klikk her for å se den komplette samlingen';
+$lang['Collection'] = 'Samling';
+$lang['Collection name:'] = 'Navn på samling';
+$lang['Collection not found'] = 'Samling ikke funnet';
+$lang['Collections'] = 'Samling';
+$lang['Create a new collection'] = 'Opprette en ny samling';
+$lang['Date added to collection, new &rarr; old'] = 'Dato lagt til samlingen, ny &rarr; gammel';
