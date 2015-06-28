5c191aae8f61ff131490ec0283ccbcbd68158ed0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-06-28 07:35:06 +0200

    Add sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,94 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You have %d collection'] = 'Имате %d збирку';
+$lang['Active'] = 'Активно';
+$lang['You have %d collections'] = 'Имате %d збирки';
+$lang['You have no collection'] = 'Немате ни једну збирку';
+$lang['Your e-mail'] = 'Ваша адреса за е-пошту';
+$lang['Your name'] = 'Ваше име';
+$lang['by %s'] = 'шаље %s';
+$lang['jump to photo'] = 'иди на слику';
+$lang['Please give a name'] = 'Дајте неки назив';
+$lang['Preview'] = 'Преглед';
+$lang['Recipient e-mail'] = 'Прималац е-поште';
+$lang['Recipient name'] = 'Име примаоца';
+$lang['Remove from collection'] = 'Уклони из збирке';
+$lang['Restricted access'] = 'Забрањен приступ';
+$lang['Save'] = 'Сачувај';
+$lang['Send'] = 'Пошаљи';
+$lang['Send this collection by mail'] = 'Пошаљи ову збирку е-поштом';
+$lang['Share'] = 'Подели';
+$lang['Share deleted'] = 'Дељење је избрисано';
+$lang['Share key'] = 'Кључ за дељење';
+$lang['Share this collection'] = 'Подели ову збирку';
+$lang['Someone else'] = 'Неко други';
+$lang['The key must be at least 8 characters long'] = 'Кључ мора да садржи бар 8 знакова';
+$lang['This collection is empty'] = 'Збирка је празна';
+$lang['This key is already used'] = 'Кључ је већ у употреби';
+$lang['This link expired'] = 'Линк је истекао';
+$lang['To'] = 'За';
+$lang['Error while sending e-mail'] = 'Грешка приликом слања е-писма';
+$lang['Expiration date'] = 'Датум истека';
+$lang['Export image list'] = 'Извези списак слика';
+$lang['Fields'] = 'Поља';
+$lang['Hello %s, %s sent you a photos collection from "%s"'] = 'Здраво %s, %s ти шаље збирку слика са „%s“';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'Здраво <b>%s</b>, %s ти шаље збирку слика са <a href="%s">%s</a>';
+$lang['Inactive'] = 'Неактивно';
+$lang['Invalid collection'] = 'Неисправна збирка ';
+$lang['Message (optional)'] = 'Порука (опционо)';
+$lang['Name'] = 'Име';
+$lang['Name, A &rarr; Z'] = 'Име, од А до Ш';
+$lang['Name, Z &rarr; A'] = 'Име, од Ш до А';
+$lang['New share added: <a href="%s">%s</a>'] = 'Додато је ново дељење: <a href="%s">%s</a>';
+$lang['Number of images'] = 'Број слика';
+$lang['Photos number, high &rarr; low'] = 'Бројеви слика, велики ка малим';
+$lang['Photos number, low &rarr; high'] = 'Бројеви слика, мали ка великим';
+$lang['Please enter the recipient e-mail'] = 'Унесите адресу примаоца е-писма';
+$lang['Please enter the recipient name'] = 'Унесите име примаоца';
+$lang['Please enter your e-mail'] = 'Унесите адресу ваше е-поште';
+$lang['Please enter your name'] = 'Унесите ваше име';
+$lang['Description'] = 'Опис';
+$lang['Download CSV file'] = 'Преузми CSV датотеку';
+$lang['E-mail sent successfully'] = 'Слање е-поруке је успело';
+$lang['Edit this collection'] = 'Уреди ову збирку';
+$lang['A photo collection by %s'] = 'Збирка слика аутора: %s';
+$lang['Add'] = 'Додај';
+$lang['Add to collection'] = 'Додај у збирку';
+$lang['Administrator'] = 'Администратор';
+$lang['Allow users to send their collections (public or not) to the admin'] = 'Дозволи корисницима да шаљу сопствене збирке (било јавне или не) администратору';
+$lang['Allow users to send their public collections by mail'] = 'Дозволи корисницима да шаљу своје јавне збирке мејлом';
+$lang['Allow users to set their collections as public'] = 'Дозволи корисницима да праве јавне збирке';
+$lang['Cancel'] = 'Откажи';
+$lang['Clear'] = 'Очисти';
+$lang['Clear this collection'] = 'Очисти ову збирку';
+$lang['Click here to view the complete collection'] = 'Кликните овде да направите комплетну збирку';
+$lang['Collection'] = 'Збирка';
+$lang['Collection name:'] = 'Име збирке:';
+$lang['Collection not found'] = 'Збирка није пронађена';
+$lang['Collections'] = 'Збирке';
+$lang['Create a new collection'] = 'Направи нову збирку';
+$lang['Date added to collection, new &rarr; old'] = 'Датум додавања у збирку, нове и старе';
+$lang['Date added to collection, old &rarr; new'] = 'Датум додавања у збирку, старе и нове';
+$lang['Delete this collection'] = 'Датум настанка збирке';
+$lang['%d more...'] = 'Још %d...';
+$lang['(remove)'] = '(уклони)';
