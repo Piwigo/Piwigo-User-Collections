a09834c1843012b52ba9b0d4092db20dbb1b68a2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-05 20:59:28 +0100

    Add ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,95 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'こんにちは <b>%s</b>、 %s があなたに写真コレクションを <a href="%s">%s</a> から送りました';
+$lang['New share added: <a href="%s">%s</a>'] = '新しい共有が追加されました：<a href="%s">%s</a>';
+$lang['by %s'] = '%s による';
+$lang['Clear'] = 'クリアする';
+$lang['Clear this collection'] = 'このコレクションをクリアする';
+$lang['You have %d collections'] = 'あなたは %d 個のコレクションを持っています';
+$lang['You have no collection'] = 'あなたはコレクションを持っていません';
+$lang['Your e-mail'] = 'あなたのEメール';
+$lang['Your name'] = 'あなたの名前';
+$lang['jump to photo'] = '写真に飛ぶ';
+$lang['Remove from collection'] = 'コレクションから削除する';
+$lang['Restricted access'] = 'アクセスが制限されています';
+$lang['Save'] = '保存する';
+$lang['Send'] = '送信する';
+$lang['Send this collection by mail'] = 'このコレクションをEメールで送信する';
+$lang['Share'] = '共有する';
+$lang['Share deleted'] = '共有が削除されました';
+$lang['Share key'] = '共有鍵';
+$lang['Share this collection'] = 'このコレクションを共有する';
+$lang['Someone else'] = '他の人';
+$lang['The key must be at least 8 characters long'] = '鍵は8文字以上でなければなりません';
+$lang['This collection is empty'] = 'このコレクションは空です';
+$lang['This key is already used'] = 'この鍵は既に使われています';
+$lang['This link expired'] = 'このリンクは有効期限を過ぎています';
+$lang['To'] = '宛先';
+$lang['You have %d collection'] = 'あなたは %d 個のコレクションを持っています';
+$lang['Invalid collection'] = '不正なコレクション';
+$lang['Message (optional)'] = 'メッセージ（オプショナル）';
+$lang['Name'] = '名前';
+$lang['Name, A &rarr; Z'] = '名前、A &rarr; Z';
+$lang['Name, Z &rarr; A'] = '名前、Z &rarr; A';
+$lang['Number of images'] = '写真の枚数';
+$lang['Photos number, high &rarr; low'] = '写真番号、高 &rarr; 低';
+$lang['Photos number, low &rarr; high'] = '写真番号、低 &rarr; 高';
+$lang['Please enter the recipient e-mail'] = '受取人のEメールを入力して下さい';
+$lang['Please enter the recipient name'] = '受取人の名前を入力して下さい';
+$lang['Please enter your e-mail'] = 'あなたのEメールを入力して下さい';
+$lang['Please enter your name'] = 'あなたの名前を入力して下さい';
+$lang['Please give a name'] = '名前を与えて下さい';
+$lang['Preview'] = 'プレビュー';
+$lang['Recipient e-mail'] = '受取人のEメール';
+$lang['Recipient name'] = '受取人の名前';
+$lang['Error while sending e-mail'] = 'Eメール送信中のエラー';
+$lang['Expiration date'] = '有効期限';
+$lang['Export image list'] = '写真リストをエクスポート';
+$lang['Fields'] = 'フィールド';
+$lang['Hello %s, %s sent you a photos collection from "%s"'] = 'こんにちは %s さん、%s が "%s" からの写真コレクションをあなたに送りました';
+$lang['Inactive'] = '非アクティブ';
+$lang['Click here to view the complete collection'] = '完全なコレクションを閲覧するにはここをクリックする';
+$lang['Collection'] = 'コレクション';
+$lang['Collection name:'] = 'コレクション名：';
+$lang['Collection not found'] = 'コレクションが見つかりません';
+$lang['Collections'] = 'コレクション';
+$lang['Create a new collection'] = '新しいコレクションを作成する';
+$lang['Date added to collection, new &rarr; old'] = 'コレクションに追加した日付、新 &rarr; 旧';
+$lang['Date added to collection, old &rarr; new'] = 'コレクションに追加した日付、旧 &rarr; 新';
+$lang['Delete this collection'] = 'このコレクションを削除する';
+$lang['Description'] = '説明';
+$lang['Download CSV file'] = 'CSVファイルをダウンロードする';
+$lang['E-mail sent successfully'] = 'Eメール送信に成功しました';
+$lang['Edit this collection'] = 'このコレクションを編集する';
+$lang['%d more...'] = '残り %d ...';
+$lang['(remove)'] = '（削除）';
+$lang['A photo collection by %s'] = '%s による写真コレクション';
+$lang['Active'] = 'アクティブ';
+$lang['Add'] = '追加する';
+$lang['Add all photos of this album to a collection'] = 'このアルバムの全ての写真をコレクションに追加する';
+$lang['Add to collection'] = 'コレクションに追加する';
+$lang['Administrator'] = '管理者';
+$lang['Allow users to send their collections (public or not) to the admin'] = 'ユーザが彼らのコレクション（パブリックであろうがなかろうが）を管理者へ送信することを許可する';
+$lang['Allow users to send their public collections by mail'] = 'ユーザがパブリックなコレクションをメールで送信することを許可する';
+$lang['Allow users to set their collections as public'] = 'ユーザが彼らのコレクションをパブリックに設定することを許可する';
+$lang['Cancel'] = '取り消す';
