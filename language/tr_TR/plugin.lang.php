69c807d5dbc5ab450fd07b73b259358d41711283
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-24 16:24:08 +0200

    Update tr_TR, thanks to kubikanber

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -93,3 +93,4 @@ $lang['Allow users to send their collections (public or not) to the admin'] = 'K
 $lang['Someone else'] = 'Başka biri';
 $lang['To'] = 'Kime';
 $lang['Add all photos of this album to a collection'] = 'Bu albümdeki tüm fotoğrafları bir kolleksiyona ekle';
+$lang['Open'] = 'Aç';
