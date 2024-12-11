ed214d2e4ca443624bb8a1bf32163abada365535
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-11 17:02:31 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,81 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Share this collection'] = 'שתף את האוסף הזה';
+$lang['Someone else'] = 'מישהו אחר';
+$lang['The key must be at least 8 characters long'] = 'המפתח חייב להיות לפחות באורך של 8 תווים';
+$lang['This collection is empty'] = 'אוסף זה ריק';
+$lang['This key is already used'] = 'מפתח זה כבר בשימוש';
+$lang['This link expired'] = 'קישור זה פג תוקף';
+$lang['To'] = 'אל';
+$lang['You have %d collection'] = 'יש לך %d אוסף';
+$lang['You have %d collections'] = 'יש לך %d אוספים';
+$lang['You have no collection'] = 'אין לך אוספים';
+$lang['Your e-mail'] = 'הדוא"ל שלך';
+$lang['Your name'] = 'שמך';
+$lang['by %s'] = 'מאת %s';
+$lang['jump to photo'] = 'דלג לתמונה';
+$lang['Number of images'] = 'מספר תמונות';
+$lang['Open'] = 'פתח';
+$lang['Photos number, high &rarr; low'] = 'מספר תמונות, גבוה &rarr; נמוך';
+$lang['Photos number, low &rarr; high'] = 'מספר תמונות, נמוך &rarr; גבוה';
+$lang['Please enter the recipient e-mail'] = 'אנא הזן את כתובת הדוא"ל של הנמען';
+$lang['Please enter the recipient name'] = 'אנא הזן את שם הנמען';
+$lang['Please enter your e-mail'] = 'אנא הזן את כתובת הדוא"ל שלך';
+$lang['Please enter your name'] = 'אנא הזן את שמך';
+$lang['Please give a name'] = 'אנא הזן שם';
+$lang['Preview'] = 'תצוגה מקדימה';
+$lang['Recipient e-mail'] = 'דוא"ל הנמען';
+$lang['Recipient name'] = 'שם הנמען';
+$lang['Remove from collection'] = 'הסר מהאוסף';
+$lang['Restricted access'] = 'גישה מוגבלת';
+$lang['Save'] = 'שמור';
+$lang['Send'] = 'שלח';
+$lang['Send this collection by mail'] = 'שלח את האוסף הזה בדוא"ל';
+$lang['Share'] = 'שתף';
+$lang['Share deleted'] = 'השיתוף נמחק';
+$lang['Share key'] = 'מפתח שיתוף';
+$lang['Date added to collection, new &rarr; old'] = 'תאריך הוספה לאוסף, חדש &rarr; ישן';
+$lang['Date added to collection, old &rarr; new'] = 'תאריך הוספה לאוסף, ישן &rarr; חדש';
+$lang['Delete this collection'] = 'מחק את האוסף הזה';
+$lang['Description'] = 'תיאור';
+$lang['Download CSV file'] = 'הורד קובץ CSV';
+$lang['E-mail sent successfully'] = 'הדוא"ל נשלח בהצלחה';
+$lang['Edit this collection'] = 'ערוך את האוסף הזה';
+$lang['Error while sending e-mail'] = 'שגיאה בעת שליחת הדוא"ל';
+$lang['Expiration date'] = 'תאריך תפוגה';
+$lang['Export image list'] = 'ייצוא רשימת תמונות';
+$lang['Fields'] = 'שדות';
+$lang['Hello %s, %s sent you a photos collection from "%s"'] = 'שלום %s, %s שלח לך אוסף תמונות מתוך "%s"';
+$lang['Hello <b>%s</b>, %s sent you a photos collection from <a href="%s">%s</a>'] = 'שלום <b>%s</b>, %s שלח לך אוסף תמונות מתוך <a href="%s">%s</a>';
+$lang['Inactive'] = 'לא פעיל';
+$lang['Invalid collection'] = 'אוסף לא חוקי';
+$lang['Message (optional)'] = 'הודעה (אופציונלי)';
+$lang['Name'] = 'שם';
+$lang['Name, A &rarr; Z'] = 'שם, א\' &rarr; ת\'';
+$lang['Name, Z &rarr; A'] = 'שם, ת\' &rarr; א\'';
+$lang['New share added: <a href="%s">%s</a>'] = 'שיתוף חדש נוסף: <a href="%s">%s</a>';
+$lang['%d more...'] = 'עוד %d...';
+$lang['(remove)'] = '(הסר)';
+$lang['A photo collection by %s'] = 'אוסף תמונות מאת %s';
+$lang['Active'] = 'פעיל';
+$lang['Add'] = 'הוסף';
+$lang['Add all photos of this album to a collection'] = 'הוסף את כל התמונות של אלבום זה לאוסף';
+$lang['Add to collection'] = 'הוסף לאוסף';
+$lang['Administrator'] = 'מנהל מערכת';
+$lang['Allow users to send their collections (public or not) to the admin'] = 'אפשר למשתמשים לשלוח את האוספים שלהם (ציבוריים או לא) למנהל';
+$lang['Allow users to send their public collections by mail'] = 'אפשר למשתמשים לשלוח את האוספים הציבוריים שלהם בדוא"ל';
+$lang['Allow users to set their collections as public'] = 'אפשר למשתמשים להגדיר את האוספים שלהם כציבוריים';
+$lang['Cancel'] = 'בטל';
+$lang['Clear'] = 'נקה';
+$lang['Clear this collection'] = 'נקה את האוסף הזה';
+$lang['Click here to view the complete collection'] = 'לחץ כאן לצפייה באוסף המלא';
+$lang['Collection'] = 'אוסף';
+$lang['Collection name:'] = 'שם האוסף:';
+$lang['Collection not found'] = 'האוסף לא נמצא';
+$lang['Collections'] = 'אוספים';
+$lang['Create a new collection'] = 'צור אוסף חדש';
