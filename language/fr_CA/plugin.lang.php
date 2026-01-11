51f0a0b940a137f9bc1ddcaa109effdb6b4b0355
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-11 05:53:16 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Please enter the recipient e-mail'] = 'Veuillez entrer le courriel du destinataire';
+$lang['Please enter your e-mail'] = 'Veuillez entrer votre courriel';
+$lang['Recipient e-mail'] = 'Courriel du destinataire';
+$lang['Send this collection by mail'] = 'Envoyer cette collection par courriel';
+$lang['Your e-mail'] = 'Votre courriel';
+$lang['E-mail sent successfully'] = 'Courriel envoyé avec succès';
+$lang['Edit this collection'] = 'Éditer cette collection';
+$lang['Error while sending e-mail'] = 'Erreur lors de l\'envoi du courriel';
+$lang['Allow users to send their public collections by mail'] = 'Les utilisateurs peuvent envoyer leurs collections publiques par courriel';
